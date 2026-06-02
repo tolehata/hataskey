4b974dc8ac40e32af61cae6a1b4cc40fae1b76cc
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-06-02 11:47:09 +0900

    fix(frontend): CSS `light-dark()` が適用されない問題を修正 (#17523)
    * fix(frontend): lightningcssのlight-darkのTranspilationを無効に (MisskeyIO#1243)
    
    * fix deps
    
    * Update Changelog
    
    ---------
    
    Co-authored-by: まっちゃてぃー。 <56515516+mattyatea@users.noreply.github.com>

--- packages/frontend-embed/vite.config.ts
+++ packages/frontend-embed/vite.config.ts
@@ -9,6 +9,7 @@ import meta from '../../package.json';
 import packageInfo from './package.json' with { type: 'json' };
 import pluginJson5 from './vite.json5.js';
 import { pluginRemoveUnrefI18n } from '../frontend-builder/rollup-plugin-remove-unref-i18n';
+import { Features } from 'lightningcss';
 
 const url = process.env.NODE_ENV === 'development' ? yaml.load(await fsp.readFile('../../.config/default.yml', 'utf-8')).url : null;
 const host = url ? (new URL(url)).hostname : undefined;
@@ -112,6 +113,9 @@ export function getConfig(): UserConfig {
 		},
 
 		css: {
+			lightningcss: {
+				exclude: Features.LightDark,
+			},
 			modules: {
 				generateScopedName(name, filename, _css): string {
 					const id = (path.relative(__dirname, filename.split('?')[0]) + '-' + name).replace(/[\\\/\.\?&=]/g, '-').replace(/(src-|vue-)/g, '');
@@ -142,9 +146,9 @@ export function getConfig(): UserConfig {
 
 		build: {
 			target: [
-				'chrome116',
-				'firefox116',
-				'safari16',
+				'chrome130',
+				'firefox132',
+				'safari18.2',
 			],
 			manifest: 'manifest.json',
 			rollupOptions: {
